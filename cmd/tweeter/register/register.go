4b8c0fa0d70ac2db87e7b22f928a7eea638ba2c9
ahaha0807 <b5331@oic.jp>
2017-12-23 19:36:10 +0900

    ユーザーアカウントを保持するファイル作成処理の追加

--- cmd/tweeter/register/register.go
+++ cmd/tweeter/register/register.go
@@ -3,11 +3,21 @@ package register
 import (
 	"github.com/urfave/cli"
 	"fmt"
+	"os"
+	"log"
 )
 
 func Register(context *cli.Context) error {
 
 	fmt.Println("register")
-	fmt.Println(context.Args().Get(0))
+
+	if _, err := os.Stat("/tmp/tweeter/user_account.txt"); os.IsNotExist(err) {
+		os.Mkdir("/tmp/tweeter", os.ModePerm)
+		_, err := os.Create("/tmp/tweeter/user_account.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	return nil
 }

06bf604ddab77ce744e86d5f7f7aeafb7677f888
ahaha0807 <b5331@oic.jp>
2017-12-23 19:47:17 +0900

    ユーザ情報保存ファイルをcsvに変更
    ファイルの取扱をしやすくするため

--- cmd/tweeter/register/register.go
+++ cmd/tweeter/register/register.go
@@ -11,9 +11,9 @@ func Register(context *cli.Context) error {
 
 	fmt.Println("register")
 
-	if _, err := os.Stat("/tmp/tweeter/user_account.txt"); os.IsNotExist(err) {
+	if _, err := os.Stat("/tmp/tweeter/user_account.csv"); os.IsNotExist(err) {
 		os.Mkdir("/tmp/tweeter", os.ModePerm)
-		_, err := os.Create("/tmp/tweeter/user_account.txt")
+		_, err := os.Create("/tmp/tweeter/user_account.csv")
 		if err != nil {
 			log.Fatal(err)
 		}

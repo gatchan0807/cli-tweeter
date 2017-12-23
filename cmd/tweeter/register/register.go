c4e7ebaa5937d4dbae1734d1dca3d0eb02091fb5
ahaha0807 <b5331@oic.jp>
2017-12-23 21:11:33 +0900

    登録しようとしたアカウントのユーザーIDの存在を確認するメソッドの追加
    同じ名前で複数回ユーザー情報を登録しないようにするため

--- cmd/tweeter/register/register.go
+++ cmd/tweeter/register/register.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"github.com/ahaha0807/cli-tweeter/cmd/tweeter/checker"
+	"io/ioutil"
+	"strings"
 )
 
 func Register(context *cli.Context) error {
@@ -17,5 +19,35 @@ func Register(context *cli.Context) error {
 		checker.Check(err)
 	}
 
+	if isExist("ahaha") {
+		fmt.Println(context.Args().Get(0) + " is already exist.")
+	}
+
 	return nil
+}
+
+func isExist(userId string) bool {
+	data, err := ioutil.ReadFile("/tmp/tweeter/user_account.csv")
+	checker.Check(err)
+
+	userIdList := convertToUserIdList(string(data))
+
+	for _, element := range userIdList {
+		if element == userId {
+			return true
+		}
+	}
+	return false
+}
+
+func convertToUserIdList(rawData string) []string {
+	var result []string
+
+	perLine := strings.Split(rawData, "\n")
+
+	for _, element := range perLine {
+		result = append(result, strings.Split(element, ",")[0])
+	}
+
+	return result
 }

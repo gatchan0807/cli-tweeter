f022ac0ab9005238c1ec91c23a138a7f559d03ff
ahaha0807 <b5331@oic.jp>
2017-12-26 09:54:53 +0900

    ユーザーのアカウントリストが存在しなかったときの処理を追加

--- cmd/tweeter/list/List.go
+++ cmd/tweeter/list/List.go
@@ -3,11 +3,25 @@ package list
 import (
 	"github.com/urfave/cli"
 	"fmt"
+	"io/ioutil"
+	"github.com/ahaha0807/cli-tweeter/cmd/tweeter/util"
 )
 
 func List(context *cli.Context) error {
 
 	fmt.Println("list")
+
+	data, err := ioutil.ReadFile("/tmp/tweeter/user_account.csv")
+	if err != nil {
+		fmt.Println("ユーザーのアカウントは登録されていませんでした。")
+		return nil
+	}
+
+	userIdList := util.ConvertToUserIdList(string(data))
+
+	for _, element := range userIdList {
+		fmt.Println(element)
+	}
 
 	return nil
 }

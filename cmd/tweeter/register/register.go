89ee904b88e18add98f3727aad0165de9761df10
ahaha0807 <b5331@oic.jp>
2017-12-26 13:26:06 +0900

    ユーザー情報の入力を受け付ける部分とCSVファイルに追記する処理の追加

--- cmd/tweeter/register/register.go
+++ cmd/tweeter/register/register.go
@@ -6,25 +6,52 @@ import (
 	"os"
 	"io/ioutil"
 	"github.com/ahaha0807/cli-tweeter/cmd/tweeter/util"
+	"golang.org/x/crypto/ssh/terminal"
+	"syscall"
 )
 
 func Register(context *cli.Context) error {
-
-	fmt.Println("register")
-
 	if _, err := os.Stat("/tmp/tweeter/user_account.csv"); os.IsNotExist(err) {
 		os.Mkdir("/tmp/tweeter", os.ModePerm)
 		_, err := os.Create("/tmp/tweeter/user_account.csv")
 		util.Check(err)
 	}
 
-	if isExist(context.Args().Get(0)) {
-		fmt.Println(context.Args().Get(0) + " is already exist.")
+	userName := getUserId()
+	userPassword := getPassword()
+	userAccountToken := getTwitterToken(userName, userPassword)
+
+	success := addToCsvFile(userName, userAccountToken)
+	if !success {
+		fmt.Println("Save failed")
 	}
 
-	//
-
 	return nil
+}
+
+func getPassword() string {
+	fmt.Println("Input yout Twitter account's Password.")
+	userPassword, err := terminal.ReadPassword(int(syscall.Stdin))
+
+	util.Check(err)
+	return string(userPassword)
+}
+
+func getUserId() string {
+	var userAccountName string
+	fmt.Println("Input your Twitter account ID.(without '@')")
+	fmt.Scan(&userAccountName)
+
+	for userAccountName == "" || isExist(userAccountName) {
+		if isExist(userAccountName) {
+			fmt.Println(userAccountName + " is already exist.")
+		}
+
+		fmt.Println("Input your Twitter account ID.(without '@')")
+		fmt.Scan(&userAccountName)
+	}
+
+	return userAccountName
 }
 
 func isExist(userId string) bool {
@@ -39,4 +66,23 @@ func isExist(userId string) bool {
 		}
 	}
 	return false
+}
+
+func addToCsvFile(accountName string, accountToken string) bool {
+	file, err := os.OpenFile("/tmp/tweeter/user_account.csv", os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return false
+	}
+
+	writeLine := accountName + "," + accountToken
+
+	fmt.Fprint(file, writeLine)
+
+	return true
+}
+
+func getTwitterToken(userAccountName string, userAccountPassword string) string {
+	token := ""
+
+	return token
 }

8075ddbaebeb77ce63bbb7a429fced96f6f214a3
ahaha0807 <b5331@oic.jp>
2018-01-01 01:02:46 +0900

    tweet機能の追加

--- cmd/tweeter/tweet/tweet.go
+++ cmd/tweeter/tweet/tweet.go
@@ -3,7 +3,13 @@ package tweet
 import (
 	"github.com/urfave/cli"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"github.com/ahaha0807/cli-tweeter/cmd/tweeter/util"
+	"github.com/ChimeraCoder/anaconda"
 )
+
+var accountListFilePath = "/tmp/tweeter/user_account.csv"
 
 func Tweet(context *cli.Context) error {
 
@@ -12,8 +18,18 @@ func Tweet(context *cli.Context) error {
 		return nil
 	}
 
-	fmt.Println("tweet")
-	fmt.Println(context.Args().Get(0))
+	data, err := ioutil.ReadFile(accountListFilePath)
+	if err != nil {
+		fmt.Println("ユーザーのアカウントは登録されていませんでした。")
+		fmt.Println("(User account not found.)")
+		return nil
+	}
+
+	userInfoList := util.ConvertToInfoList(string(data))
+	tweetAccount := userInfoList[0]
+
+	err = doTweet(tweetAccount, context.Args().Get(0))
+	util.Check(err)
 
 	return nil
 }
@@ -26,4 +42,21 @@ func TweetWithAccount(context *cli.Context, accountId string) error {
 	fmt.Println(context.Args().Get(0))
 
 	return nil
+}
+
+func doTweet(tweetAccount map[string]string, tweetContents string) error {
+	anaconda.SetConsumerKey(os.Getenv("TWITTER_CLI_CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CLI_CONSUMER_SECRET_KEY"))
+
+	api := anaconda.NewTwitterApi(tweetAccount["accessToken"], tweetAccount["accessSecret"])
+
+	tweet, err := api.PostTweet(tweetContents, nil)
+	fmt.Print("[Tweet Successed] ")
+	fmt.Println(tweet.Text)
+
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
